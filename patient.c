78b3d8a565cee0e26b4d280e2078f40c5a3c46b9
Livia Hombre <152039280+liviahombre@users.noreply.github.com>
2024-08-21 21:34:46 -0300

    Update patient.c

--- patient.c
+++ patient.c
@@ -4,7 +4,6 @@
 #include <time.h>
 #include "patient.h"
 
-/* teste */
 
 Patient* create_patient(int id, const char *name, struct tm *timestamp) {
     Patient *new_patient = (Patient*)malloc(sizeof(Patient));
