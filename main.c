96cfcb05c1cfa3b0f0e71637da9b1fe83882e28f
Thiago <thiagoffabiano@gmail.com>
2024-08-25 10:57:53 -0300

    Adiciona elementos que faltavam no fluxo

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 #include "patient.h"
 #include "XRMachineManager.h"
 #include "IA.h"
+#include "ExamPriorityQueue.h"
+#include "report.h"
 
 int main() {
 
@@ -17,9 +19,12 @@ int main() {
 
     int id = 1;
     Queue* filaDePacientes = create_queue_patient();
+    Queue* filaDeExamesPorPrioridade = create_queue_exam()
     MachineManager* gerenciadorDeMaquinas = criar_XRManager();
     char nomePaciente[50];
 
+
+    //Loop para criar a fila de pacientes
     while (1) {  
         struct tm timestamp;
         time_t t = time(NULL);
@@ -32,19 +37,34 @@ int main() {
             Patient* paciente = create_patient(id, nomePaciente, &timestamp); 
             enqueue_patient(filaDePacientes, paciente);         
             id++;
+            
         } else {
             printf("Nao chama paciente\n"); 
         };
         Sleep(unidadeDeTempo);  
     }
 
+    //Loop para a realização dos exames
     while(1){
         int maquinaAlocadaId = alocar_paciente(gerenciadorDeMaquinas, filaDePacientes);
         Exam *exame = realizar_exame(maquinaAlocadaId, paciente->id);
-        Sleep(unidadeDeTempo * 10);  
+        Sleep(unidadeDeTempo * 10); 
+        liberar_maquina(gerenciadorDeMaquinas, maquinaAlocadaId);
+        enqueue_exam(filaDeExamesPorPrioridade, exame);
     }
 
-    
+    //Loop para realização dos laudos
+    while(1){
+        Sleep(unidadeDeTempo * 30); 
+        Report* laudo = criar_laudo(filaDeExamesPorPrioridade)
+    }
+
+    //Loop para imprimir retlatório
+    while (1) {  
+        printf("Numero de pacientes que visitaram o hospital: %d", id);
+        printf("Numero de pacientes na fila aguardando o exame: %d", id);
+
+    }
 
     return 0;
 }
